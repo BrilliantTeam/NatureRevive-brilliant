88e250b87369e90a062cd771b628e06743ef7fca
RICE0707 <master.end.9377@gmail.com>
2026-06-01 23:06:29 +0800

    為小白準備的編譯環境的改善

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 include("naturerevive-common")
 
 listOf("1_17", "1_18", "1_18_2", "1_19", "1_19_1", "1_19_3", "1_19_4", "1_20_1", "1_20_2", "1_20_4", "1_20_6",
